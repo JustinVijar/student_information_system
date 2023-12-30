4afed98296eb99da8e0752b8ea18ed7097aa66bf
vincentnator <145983844+vincentnator@users.noreply.github.com>
2023-12-30 19:29:00 +0800

    ViewCourse.java

--- src/student_information_system/ViewCourse.java
+++ src/student_information_system/ViewCourse.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package gui_calculator;
+package student_information_system;
 
 import java.beans.Statement;
 import static java.lang.reflect.Array.set;
